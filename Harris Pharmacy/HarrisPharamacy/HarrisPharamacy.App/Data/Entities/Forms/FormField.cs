a5716f5addfdece1893531e8b2686083267e3588
Taylor Adam <tjadam1993@gmail.com>
2019-10-15 13:41:12 -0700

    Added input type to form fields

--- Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Data/Entities/Forms/FormField.cs
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Data/Entities/Forms/FormField.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HarrisPharmacy.App.Data.Enums;
 
 namespace HarrisPharmacy.App.Data.Entities.Forms
 {
@@ -24,5 +25,10 @@ namespace HarrisPharmacy.App.Data.Entities.Forms
         /// The form this form field belongs to - reference navigation property
         /// </summary>
         public Form Form { get; set; }
+
+        /// <summary>
+        /// The type of input
+        /// </summary>
+        public FormInputType FormInputType { get; set; }
     }
 }
--- Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Migrations/ApplicationDb/20191015204009_formFieldType.Designer.cs
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Migrations/ApplicationDb/20191015204009_formFieldType.Designer.cs
@@ -4,14 +4,16 @@ using HarrisPharmacy.App.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HarrisPharmacy.App.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191015204009_formFieldType")]
+    partial class formFieldType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
