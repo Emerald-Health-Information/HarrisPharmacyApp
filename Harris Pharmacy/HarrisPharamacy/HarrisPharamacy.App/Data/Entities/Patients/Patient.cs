c8148c554b6adcf75517eece060b01524285fac8
GraysonKing <grayson.king065@gmail.com>
2019-10-21 14:27:32 -0700

    Patient database objects, again (merge from master deleted it all)

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Data/Entities/Patients/Patient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisPharmacy.App.Data.Entities.Patients
+{
+    /// <summary>
+    /// Represents a patient
+    /// </summary>
+    public class Patient : BaseEntity
+    {
+        /// <summary>
+        /// Primary key for patients
+        /// </summary>
+        public string PatientId { get; set; }
+
+        /// <summary>
+        /// First name of a patient
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Last name of a patient
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Age of a patient
+        /// </summary>
+        public string Age { get; set; }
+
+        /// <summary>
+        /// Mental status of a patient
+        /// </summary>
+        public string MentalStatus { get; set; }
+
+        /// <summary>
+        /// Sex of patient
+        /// </summary>
+        public string Sex { get; set; }
+    }
+}
---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Data/Entities/Patients/PatientList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisPharmacy.App.Data.Entities.Patients
+{
+    /// <summary>
+    /// Represents a single appointment in a list of patients
+    /// </summary>
+    public class PatientList : BaseEntity
+    {
+        /// <summary>
+        /// Primary key for a patient list item
+        /// </summary>
+        public string PatientListId { get; set; }
+        /// <summary>
+        /// Foreign key to patients
+        /// </summary>
+        public string PatientId { get; set; }
+        /// <summary>
+        /// Foreign key to users
+        /// </summary>
+        public string UserId { get; set; }
+        /// <summary>
+        /// Starting time for the appointment
+        /// </summary>
+        public string StartTime { get; set; }
+        /// <summary>
+        /// Ending time for the appointment
+        /// </summary>
+        public string EndTime { get; set; }
+        /// <summary>
+        /// Location for the appointment
+        /// </summary>
+        public string Location { get; set; }
+    }
+}
