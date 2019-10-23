843abe60a287aa9ac5e378e1c43e35afde8de00d
Taylor Adam <tjadam1993@gmail.com>
2019-10-23 14:09:27 -0700

    forgot to add this to last commit

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharmacy.UnitTests/UnitTest.cs
@@ -0,0 +1,20 @@
+using System;
+using HarrisPharmacy.App.Data.Interfaces;
+using HarrisPharmacy.App.Data.Services;
+using Xunit;
+
+namespace HarrisPharmacy.UnitTests
+{
+    public class MathServicesTests
+    {
+        private readonly MathServices _mathService = new MathServices();
+
+        [Fact]
+        public void IncrementCountTestReturnCountPlusOne()
+        {
+            var currentCount = 0;
+
+            Assert.Equal(1, _mathService.IncrementCount(currentCount));
+        }
+    }
+}
