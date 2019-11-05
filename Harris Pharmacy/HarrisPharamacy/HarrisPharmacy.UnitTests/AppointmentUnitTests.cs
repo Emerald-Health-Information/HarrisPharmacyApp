f60a61e24455e5f7be7ca6e8546526e9d20038a7
GraysonKing <grayson.king065@gmail.com>
2019-11-05 14:38:44 -0800

    added appointment unit tests

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharmacy.UnitTests/AppointmentUnitTests.cs
@@ -0,0 +1,34 @@
+﻿using HarrisPharmacy.App.Data;
+using HarrisPharmacy.App.Data.Entities.Patients;
+using HarrisPharmacy.App.Data.Interfaces;
+using HarrisPharmacy.App.Data.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace HarrisPharmacy.UnitTests
+{
+    public class AppointmentUnitTests
+    {
+        private readonly IAppointmentService _appointmentService;
+        public AppointmentUnitTests()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(databaseName: "temp")
+                          .Options;
+            var dbContext = new ApplicationDbContext(options);
+            _appointmentService = new AppointmentService(dbContext);
+        }
+        
+        /// <summary>
+        /// Queries database to check that there are appointments in the database.
+        /// </summary>
+        [Fact]
+        public async void GetAllAppointmentsTest()
+        {
+            Assert.NotNull(await _appointmentService.GetPatientListAsync());
+        }
+    }
+}
