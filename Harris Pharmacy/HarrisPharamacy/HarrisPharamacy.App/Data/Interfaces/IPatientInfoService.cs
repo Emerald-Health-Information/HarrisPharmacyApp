6f169e7e3f0f0214fbf673e9d7a55ad149a3697d
BatesJackson <bates677@gmail.com>
2019-11-19 16:01:28 -0800

    Added patient info service

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Data/Interfaces/IPatientInfoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HarrisPharmacy.App.Data.Entities.Patients;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace HarrisPharmacy.App.Data.Interfaces
+{
+    /// <summary>
+    /// The interface for the service class for appointments that allows the front end to interact with the database
+    /// </summary>
+    public interface IPatientInfoService
+    {
+        Task<Patient> GetPatientInformationAsync(String patientListID);
+    }
+}
