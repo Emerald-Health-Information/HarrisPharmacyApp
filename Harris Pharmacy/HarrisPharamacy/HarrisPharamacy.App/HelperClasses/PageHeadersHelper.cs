56d7453c0276827f6dfab014515d271a16dce77d
Taylor Adam <tjadam1993@gmail.com>
2019-12-02 17:27:52 -0800

    Added page url helper class and page name to top of page

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/HelperClasses/PageHeadersHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace HarrisPharmacy.App.HelperClasses
+{
+    public static class PageHeaderHelper
+    {
+        public static string GetPageHeader(string value)
+        {
+            value = value.Remove(0, value.LastIndexOf("/", StringComparison.Ordinal));
+            switch (value)
+            {
+                case "/":
+                    return "Appointments";
+
+                case "/appointment":
+                    return "Create Appointment";
+
+                case "/formBuilder":
+                    return "Edit Form Builder";
+
+                case "/FormFields":
+                    return "Edit Form Fields";
+
+                default:
+                    return "Home";
+            }
+        }
+    }
+}
