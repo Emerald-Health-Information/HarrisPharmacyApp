6a962587c3c3e21ec1399880a343557f29999f25
Taylor Adam <tjadam1993@gmail.com>
2020-02-02 18:38:57 -0800

    Added api project, added a sample controller so people know how it works

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharmacy.API/Controllers/FormsAPI/FormsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using HarrisPharmacy.Data.Entities.Forms;
+using HarrisPharmacy.Data.Interfaces;
+using HarrisPharmacy.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HarrisPharmacy.API.Controllers.FormsAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormFieldController : ControllerBase
+    {
+        private readonly IFormService _formService;
+
+        public FormFieldController(IFormService formService)
+        {
+            this._formService = formService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<FormField>>> GetAllFormsAsync()
+        {
+            //List<Form> forms = await _formService.GetFormsAsync();
+            //return forms;
+            return await _formService.GetFormFieldsAsync();
+        }
+    }
+}
---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharmacy.API/Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HarrisPharmacy.App.Areas.Identity;
+using HarrisPharmacy.Data;
+using HarrisPharmacy.Data.Interfaces;
+using HarrisPharmacy.Data.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace HarrisPharmacy.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AuthDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("AuthContextConnection")), (ServiceLifetime.Transient));
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddEntityFrameworkStores<AuthDbContext>();
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("ApplicationDbConnectionString")), (ServiceLifetime.Transient));
+
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+
+            services.AddControllers();
+            services.AddTransient<IFormService, FormService>();
+            services.AddTransient<IAppointmentService, AppointmentService>();
+            services.AddTransient<IPatientInfoService, PatientInfoService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
