3e16416dc498e693f0434183e78b2cf185ce3d4e
BatesJackson <32694371+BatesJackson@users.noreply.github.com>
2020-02-19 10:08:11 -0800

    start appoinment controller

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharmacy.API/Controllers/AppointmentsAPI/AppointmentsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HarrisPharmacy.Data.Entities.Appointments;
+using HarrisPharmacy.Data.Interfaces;
+using HarrisPharmacy.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HarrisPharmacy.API.Controllers.AppointmentsAPI
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentsController(IAppointmentService appointmentService)
+        {
+            this._appointmentService = appointmentService;
+        }
+
+        [HttpGet("appointment/{appointmentId}")]       
+        public async Task<Appointment> GetAppointmentAsync(string appointmentId)
+        {
+            return await _appointmentService.GetAppointmentAsync(appointmentId);
+        }
+
+        [HttpGet("list")]
+        public async Task<List<Appointment>> GetPatientListAsync()
+        {
+            return await _appointmentService.GetPatientListAsync();
+   
+        }
+    }
+}
