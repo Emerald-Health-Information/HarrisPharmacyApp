9724e064db9a1f5bbed36d79146e69878b61cac5
GraysonKing <grayson.king065@gmail.com>
2020-03-22 15:35:31 -0700

    Move Fido Demo into main app

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Models/RegistrationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarrisPharmacy.App.Models
+{
+   public class RegistrationModel
+   {
+      public string UserId { get; set; }
+      public string DeviceName { get; set; }
+   }
+}
