9fd6f090ca5d32c09cc5ca3f90e669eaae259d00
Brandon Chesley <brandon.chesley47@gmail.com>
2020-03-23 12:04:53 -0700

    Added Fido2 files to App Project

---
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/Models/RegistrationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+   public class RegistrationModel
+   {
+      public string UserId { get; set; }
+   }
+
