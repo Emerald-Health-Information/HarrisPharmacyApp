358cfd7ededee9350b7edc9a5e57499f6400774c
Taylor Adam <tjadam1993@gmail.com>
2020-04-22 22:21:08 -0700

    Fixed day times

--- Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/wwwroot/js/app.js
+++ Harris Pharmacy/HarrisPharamacy/HarrisPharamacy.App/wwwroot/js/app.js
@@ -56,3 +56,29 @@ function toggleNav() {
         openNav();
     }
 }
+
+var LastElementDOM;
+var LastValueDOM;
+
+function mutateDOM(ElementDOM, value) {
+    if (LastElementDOM == ElementDOM && LastValueDOM == value)
+        return false;
+
+    LastElementDOM = ElementDOM;
+    LastValueDOM = value;
+
+    var event = new Event('change');
+    ElementDOM.dispatchEvent(event);
+}
+
+function selectChange(_this) {
+    mutateDOM(_this, _this.value);
+    return "Done";
+}
+
+function getStartDate() {
+    return $('#datetimepicker1').datetimepicker('viewDate');
+}
+function getEndDate() {
+    return $('#datetimepicker2').datetimepicker('viewDate');
+}
